e0dd55fbe8bf2985fc34d309475c8b8e90993ddd
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-11-08 11:46:30 -0500

    Added edge cases to stack op testing.

--- integration/standard_library_tests.rb
+++ integration/standard_library_tests.rb
@@ -33,19 +33,23 @@ class StandardLibraryTester < MiniTest::Unit::TestCase
     foorth_equal("1 2", [1, 2])
 
     foorth_equal("1 2 drop", [1])
+    foorth_raises("drop")
 
     foorth_equal("33 dup", [33,33])
+    foorth_raises("dup")
 
     foorth_equal("false ?dup", [false])
     foorth_equal("nil ?dup", [nil])
     foorth_equal('"" ?dup', [""])
     foorth_equal("0 ?dup", [0])
-
     foorth_equal("true  ?dup", [true, true])
     foorth_equal('"A" ?dup', ["A", "A"])
     foorth_equal("1 ?dup", [1,1])
+    foorth_raises("?dup")
 
     foorth_equal("1 2 swap", [2,1])
+    foorth_raises("1 swap")
+    foorth_raises("swap")
 
     foorth_equal("1 2 3 rot", [2,3,1])
 
@@ -54,10 +58,14 @@ class StandardLibraryTester < MiniTest::Unit::TestCase
     foorth_equal("1 2 3 1 pick", [1,2,3,3])
     foorth_equal("1 2 3 2 pick", [1,2,3,2])
     foorth_equal("1 2 3 3 pick", [1,2,3,1])
+    foorth_raises("1 2 3 4 pick")
 
     foorth_equal("1 2 nip", [2])
+    foorth_raises("1 nip")
 
     foorth_equal("1 2 tuck", [2,1,2])
+    foorth_raises("1 tuck")
+    foorth_raises("tuck")
   end
 
   def test_some_computations
