53701dcccf18ededb04c08544ae8b474c3ebdd5f
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-11-22 14:42:01 -0500

    Added .lj, .cj, and .rj too string library.

---
+++ integration/string_library_tests.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+require_relative '../lib/fOOrth'
+require_relative 'support/foorth_testing'
+require          'minitest/autorun'
+
+#Test the standard fOOrth library.
+class StringLibraryTester < MiniTest::Unit::TestCase
+
+  include XfOOrthTestExtensions
+
+  #Special initialize to track rake progress.
+  def initialize(*all)
+    $do_this_only_one_time = "" unless defined? $do_this_only_one_time
+
+    if $do_this_only_one_time != __FILE__
+      puts
+      puts "Running test file: #{File.split(__FILE__)[1]}"
+      $do_this_only_one_time = __FILE__
+    end
+
+    super(*all)
+  end
+
+  def test_left_justification
+    foorth_equal('"a" 5 .lj ', ['a    '])
+    foorth_equal('"too long" 5 .lj ', ['too long'])
+  end
+
+  def test_center_justification
+    foorth_equal('"a" 5 .cj ', ['  a  '])
+    foorth_equal('"too long" 5 .cj ', ['too long'])
+  end
+
+  def test_right_justification
+    foorth_equal('"a" 5 .rj ', ['    a'])
+    foorth_equal('"too long" 5 .rj ', ['too long'])
+  end
+end
--- lib/fOOrth/library/string_library.rb
+++ lib/fOOrth/library/string_library.rb
@@ -6,4 +6,16 @@ module XfOOrth
   #Connect the String class to the fOOrth class system.
   String.create_foorth_proxy
 
+  # [a n] .lj ['a    ']
+  VirtualMachine.create_shared_method('.lj', VmSpec, [],
+    &lambda {|vm| width = pop.to_i;  poke(peek.to_s.ljust(width)); })
+
+  # [a n] .cj ['  a  ']
+  VirtualMachine.create_shared_method('.cj', VmSpec, [],
+    &lambda {|vm| width = pop.to_i;  poke(peek.to_s.center(width)); })
+
+  # [a n] .rj ['    a']
+  VirtualMachine.create_shared_method('.rj', VmSpec, [],
+    &lambda {|vm| width = pop.to_i;  poke(peek.to_s.rjust(width)); })
+
 end
