9469df82951d4cc083b5ddd05b931ffee532b4ba
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-12-02 17:35:30 -0500

    Massaged some comments.

--- lib/fOOrth/library/array_library.rb
+++ lib/fOOrth/library/array_library.rb
@@ -1,11 +1,13 @@
 # coding: utf-8
 
-
 #* library/array_library.rb - Numeric support for the fOOrth library.
 module XfOOrth
 
   #Connect the Array class to the fOOrth class system.
   Array.create_foorth_proxy
+
+  # [] Array .new [[]]; create an array of n zeros.
+  # The default implementation from Object is used for this.
 
   # [n] Array .new_size [[0,0,...0]]; create an array of n zeros.
   Array.create_exclusive_method('.new_size', TosSpec, [],
@@ -32,12 +34,12 @@ end
 #* Runtime library support for fOOrth constructs.
 class Array
 
-  # Support for the .new{ } construct.
+  # Runtime support for the .new{ } construct.
   def self.do_foorth_new_block(vm, &block)
     Array.new(vm.pop(), &block)
   end
 
-  # Support for the .each{ } construct.
+  # Runtime support for the .each{ } construct.
   def do_foorth_each(&block)
     self.each_with_index(&block)
   end
