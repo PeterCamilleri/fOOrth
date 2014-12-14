189dba5b22c317936fdeb43efd0e303eb0ce43ad
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-12-14 09:51:16 -0500

    Corrected for syntax highlighting bug.

--- lib/fOOrth/library/stubs.rb
+++ lib/fOOrth/library/stubs.rb
@@ -44,7 +44,7 @@ module XfOOrth
   Object.create_shared_method('do_each', TosSpec, [:stub])
 
   #Define some "crossover" symbols.
-  SymbolMap.add_entry('.is_class?', :foorth_is_class?)
+  SymbolMap.add_entry('.is_class?', "foorth_is_class?".to_sym)
 
   SymbolMap.add_entry('.to_s',      :to_foorth_s)
   SymbolMap.add_entry('.strlen',    :foorth_strlen)
