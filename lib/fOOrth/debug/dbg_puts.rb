1833937a9f0e465513177e17ca7e91cb4ac95962
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-05 16:17:34 -0500

    Added rdoc docs for the dbg_puts method.

--- lib/fOOrth/debug/dbg_puts.rb
+++ lib/fOOrth/debug/dbg_puts.rb
@@ -5,6 +5,8 @@ module XfOOrth
 
   #* dbg_puts.rb - Display diagnostic/debug information if enabled.
   class VirtualMachine
+
+    #Send out debug info to the fOOrth debug port if debug is enabled.
     def dbg_puts(*args)
       $foorth_dbg.puts(*args) if debug
     end
