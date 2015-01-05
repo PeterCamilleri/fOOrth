8b0a8abdc68722d1774b83d2cd0c90acd39157c7
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-04 21:09:33 -0500

    Corrected comment for Hash literals.

--- lib/fOOrth/library/hash_library.rb
+++ lib/fOOrth/library/hash_library.rb
@@ -6,7 +6,7 @@ module XfOOrth
   #Connect the Array class to the fOOrth class system.
   Hash.create_foorth_proxy
 
-  # [] { k1 v1 -> ... kn vn -> } [[k1=>v1,...kn=>vn]]; a hash literal value
+  # [] { k1 v1 -> ... kn vn -> } [{k1=>v1,...kn=>vn}]; a hash literal value
   VirtualMachine.create_shared_method('{', VmSpec, [:immediate], &lambda { |vm|
     vm.suspend_execute_mode('vm.push(Hash.new); ', :hash_literal)
 
