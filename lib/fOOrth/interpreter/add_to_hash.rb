a98f779bfd403e3102179e4fa04fcfd1b8fbd2f2
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-05 16:18:57 -0500

    Corrected docs for the add_to_hash method.

--- lib/fOOrth/interpreter/add_to_hash.rb
+++ lib/fOOrth/interpreter/add_to_hash.rb
@@ -6,8 +6,7 @@ module XfOOrth
   #* interpreter/add_to_hash.rb - The fOOrth language hash literal support module.
   class VirtualMachine
 
-    #Compress all the added entries into a single entry and revive the previous
-    #contents of the data stack.
+    #Add the key and value to the hash being constructed.
     def add_to_hash
       key, value = popm(2)
       peek(1)[key] = value
