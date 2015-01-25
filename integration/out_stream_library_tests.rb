2daac0e00f5cc5995eaf0189250f462847c27ac5
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-24 21:32:34 -0500

    OutStream initializer and stubbed out .new method.

--- integration/out_stream_library_tests.rb
+++ integration/out_stream_library_tests.rb
@@ -13,10 +13,15 @@ class OutStreamLibraryTester < MiniTest::Unit::TestCase
   #Track mini-test progress.
   MinitestVisible.track self, __FILE__
 
+  $osfn = '"integration/out_stream_test_1.txt" '
+
   def test_that_class_exists
     foorth_equal('OutStream',       [XfOOrth::XfOOrth_OutStream])
     foorth_equal('OutStream .name', ['OutStream'])
   end
 
+  def test_that_new_is_a_stub
+    foorth_raises('OutStream .new')
+  end
 
 end
--- lib/fOOrth/library/out_stream_library.rb
+++ lib/fOOrth/library/out_stream_library.rb
@@ -11,6 +11,20 @@ module XfOOrth
     #The file used to perform the actual input operations.
     attr_reader :file
 
+    #Set up the OutStream for the given file name.
+    #<br>Parameters
+    #* file_name - The name of the file.
+    #* file_mode - The mode to use opening the file. Either 'w' or 'a'
+    def initialize(file_name, file_mode)
+      @file = File.new(file_name, file_mode)
+    rescue
+      error "Unable to open the file #{file_name} for writing."
+    end
+
   end
+
+  #The .new method is stubbed out.
+  out_stream.create_exclusive_method('.new', TosSpec, [:stub])
+
 
 end
