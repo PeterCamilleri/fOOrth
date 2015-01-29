ef0c64057e75d3188a1c3797b96a995a04027d47
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-29 14:05:57 -0500

    Smells cleanup.

--- lib/fOOrth/debug/vm_dump.rb
+++ lib/fOOrth/debug/vm_dump.rb
@@ -8,17 +8,18 @@ module XfOOrth
 
     #Dump the virtual machine to the console for debug.
     def debug_dump
-      puts "\n#{self.foorth_name}"
-      puts "  Ruby    = #{self.to_s}"
-      puts "  Stack   = #{@data_stack.inspect}"
-      puts "  Nesting = #{@context.depth}"
-      puts "  Quotes  = #{@quotes}"
-      puts "  Debug   = #{@debug}"
-      puts "  Show    = #{@show_stack}"
-      puts "  Force   = #{@force}"
-      puts "  Start   = #{@start_time}"
-      puts "  Source  = #{@parser.source.source_name}"
-      puts "  Buffer  = #{@parser.source.read_buffer.inspect}"
+      source = @parser.source
+      puts "\n#{self.foorth_name}"                +
+           "\n  Ruby    = #{self.to_s}"           +
+           "\n  Stack   = #{@data_stack.inspect}" +
+           "\n  Nesting = #{@context.depth}"      +
+           "\n  Quotes  = #{@quotes}"             +
+           "\n  Debug   = #{@debug}"              +
+           "\n  Show    = #{@show_stack}"         +
+           "\n  Force   = #{@force}"              +
+           "\n  Start   = #{@start_time}"         +
+           "\n  Source  = #{source.source_name}"  +
+           "\n  Buffer  = #{source.read_buffer.inspect}"
     end
 
   end
