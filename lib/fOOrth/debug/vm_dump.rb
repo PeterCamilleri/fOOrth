904a8134f3ef06d6175abfd2917ab7f98c0c16ee
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-01-30 11:30:47 -0500

    Long string tweak.

--- lib/fOOrth/debug/vm_dump.rb
+++ lib/fOOrth/debug/vm_dump.rb
@@ -9,16 +9,16 @@ module XfOOrth
     #Dump the virtual machine to the console for debug.
     def debug_dump
       source = @parser.source
-      puts "\n#{self.foorth_name}"                +
-           "\n  Ruby    = #{self.to_s}"           +
-           "\n  Stack   = #{@data_stack.inspect}" +
-           "\n  Nesting = #{@context.depth}"      +
-           "\n  Quotes  = #{@quotes}"             +
-           "\n  Debug   = #{@debug}"              +
-           "\n  Show    = #{@show_stack}"         +
-           "\n  Force   = #{@force}"              +
-           "\n  Start   = #{@start_time}"         +
-           "\n  Source  = #{source.source_name}"  +
+      puts "\n#{self.foorth_name}"                \
+           "\n  Ruby    = #{self.to_s}"           \
+           "\n  Stack   = #{@data_stack.inspect}" \
+           "\n  Nesting = #{@context.depth}"      \
+           "\n  Quotes  = #{@quotes}"             \
+           "\n  Debug   = #{@debug}"              \
+           "\n  Show    = #{@show_stack}"         \
+           "\n  Force   = #{@force}"              \
+           "\n  Start   = #{@start_time}"         \
+           "\n  Source  = #{source.source_name}"  \
            "\n  Buffer  = #{source.read_buffer.inspect}"
     end
 
