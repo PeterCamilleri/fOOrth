ee6815db5666c466bb8e1bf325b20732ea019385
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-02-28 09:39:16 -0500

    Added TODO item.

--- lib/fOOrth/compiler/process.rb
+++ lib/fOOrth/compiler/process.rb
@@ -57,7 +57,7 @@ module XfOOrth
     #* token - The token to receive the generated code.
     #* word  - The text of the word.
     def generate_code(token, word)
-      unless word == '"'
+      unless word == '"'   # TODO -- normalize this test away.
         if (spec = @context.map(word))
           spec.build_on(token)
         elsif (value = word.to_foorth_n)
