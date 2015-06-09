791ff241b2321ca55ebdeac51c5b2639bf1a063b
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-06-08 21:07:14 -0400

    Code refactor: Split up parser.rb

---
+++ lib/fOOrth/compiler/parser/get_string.rb
@@ -0,0 +1,81 @@
+# coding: utf-8
+
+#* compiler/parser/get_string.rb - Extract string literals from code source.
+module XfOOrth
+
+  #* parser.rb - Parse source code from a code source.
+  class Parser
+
+    #Get the balance of a string from the source code source.
+    def get_string
+      vm = Thread.current[:vm]
+      vm.quotes, done, result = 1, false, ''
+
+      begin
+        next_char = @source.get
+
+        if next_char == "\\"
+          result << process_backslash
+        elsif next_char == '"'
+          vm.quotes, done = 0, true
+        elsif @source.eoln?
+          vm.quotes, done = 0, true
+        elsif next_char >= ' '
+          result << next_char
+        end
+      end until done
+
+      result
+    end
+
+    #Process a backlash character found with a string in the source text.
+    def process_backslash
+      next_char = @source.get
+
+      if next_char == ' ' && @source.eoln?
+        next_char = skip_white_space_or_to_eoln
+      elsif next_char == 'n'
+        next_char = "\n"
+      elsif next_char == 'x'
+        next_char = process_8_bit
+      elsif next_char == 'u'
+        next_char = process_16_bit
+      elsif next_char != "\\" && next_char != '"'
+        error "F10: Invalid string literal value: '\\#{next_char}'"
+      end
+
+      next_char
+    end
+
+    #Process an 8 bit hex character constant.
+    def process_8_bit
+      hex = process_hex_character +
+            process_hex_character
+
+      eval("\"\\x#{hex}\"")
+    end
+
+    #Process a 16 bit hex character constant.
+    def process_16_bit
+      hex = process_hex_character +
+            process_hex_character +
+            process_hex_character +
+            process_hex_character
+
+      eval("\"\\u#{hex}\"")
+    end
+
+    #Get a hex character from the input stream.
+    def process_hex_character
+      next_char = @source.get
+
+      unless "0123456789ABCDEFabcdef".include?(next_char)
+        error "F10: Invalid hex character: '#{next_char}'"
+      end
+
+      next_char
+    end
+
+  end
+
+end
---
+++ lib/fOOrth/compiler/parser/special.rb
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+#* compiler/parser/special.rb - Parse source code in special quoted mode.
+module XfOOrth
+
+  #* parser.rb - Parse source code from a code source.
+  class Parser
+
+    #Get the next forth word and any embedded string data. This is used to
+    #support the quoted compiler mode.
+    #<br>Returns:
+    #* A string with the next non-comment language element or nil if none
+    #  could be found.
+    def get_word_or_string
+      return nil unless (word = get_word)
+
+      if word[-1] == '"'
+        vm = Thread.current[:vm]
+        vm.quotes, skip, done = 1, false, false
+
+        until done
+          return nil unless (next_char = @source.get)
+          word << next_char
+
+          if skip
+            skip = false
+          elsif next_char == '"'
+            vm.quotes, done = 0, true
+          else
+            skip = (next_char == '\\')
+          end
+        end
+      end
+
+      word
+    end
+
+  end
+
+end
