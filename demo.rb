ae65dfa815973c59662009ee0e92a4e68458361f
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-08-14 11:21:00 -0400

    Demo now detects if fOOrth is already loaded.

--- demo.rb
+++ demo.rb
@@ -2,12 +2,16 @@
 
 #This file contains a minimum host environment for running the fOOrth system.
 
-begin
-  require 'fOOrth'
-  puts "\nLoaded fOOrth from the system gem."
-rescue LoadError
-  require './lib/fOOrth'
-  puts "\nLoaded fOOrth from the local code folder."
+if defined?(XfOOrth)
+  puts "The fOOrth system is already loaded."
+else
+  begin
+    require 'fOOrth'
+    puts "\nLoaded fOOrth from the system gem."
+  rescue LoadError
+    require './lib/fOOrth'
+    puts "\nLoaded fOOrth from the local code folder."
+  end
 end
 
 puts
