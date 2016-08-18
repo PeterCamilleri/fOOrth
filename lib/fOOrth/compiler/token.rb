c8d4464f2c8caeaf989a761a5f719cbf84a0265d
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-08-18 19:16:11 -0400

    Clarified token add parameters.

--- lib/fOOrth/compiler/token.rb
+++ lib/fOOrth/compiler/token.rb
@@ -16,13 +16,26 @@ module XfOOrth
     end
 
     #Append some text/tags to the code_fragment.
+    #<br>Parameters
+    #* text - A string of code to be appended to this token.
+    #* tags - An optional array of tags to be added to this token.
+    #<br>Possible tag values:
+    #* :class - This token contains a class constant..
+    #* :immediate - The token is executed, even in compile modes.
+    #* :macro - This token contains an in-line macro.
+    #* :numeric - This token contains a numeric literal.
+    #* :procedure - This token contains a procedure literal.
+    #* :string - This token contains a string literal.
+    #* :stub - This token contains a stub spec.
+    #* :temp - This token contains code from a temporary spec.
+    #* none - Nothing special here. Move along, move along.
     def add(text, tags=nil)
       @code << text
       @tags.concat(tags).uniq! if tags
       self
     end
 
-    #Does this token have the specified tag?
+    #Does this token have the specified tag value?
     def has_tag?(value)
       @tags.include?(value)
     end
