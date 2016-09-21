e20abff99ccfb406327c96a84508d544e6bae276
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-09-21 15:21:49 -0400

    Cleanup of introspection string.rb

--- lib/fOOrth/library/introspection/string.rb
+++ lib/fOOrth/library/introspection/string.rb
@@ -4,6 +4,8 @@
 class String
 
   #Scan all classes for information about a method.
+  #<br>Endemic Code Smells
+  #* :reek:DuplicateMethodCall :reek:FeatureEnvy :reek:TooManyStatements
   def foorth_method_scan
     symbol, results = XfOOrth::SymbolMap.map_info(self)
     found = false
@@ -11,13 +13,27 @@ class String
     symbol && $FOORTH_GLOBALS.values
       .select {|entry| entry.has_tag?(:class)}
       .collect {|spec| spec.new_class}
-      .sort {|a,b| a.foorth_name <=> b.foorth_name}
+      .sort {|first, second| first.foorth_name <=> second.foorth_name}
       .each do |klass|
-        spec, info = klass.map_foorth_shared_info(symbol, :shallow)
-        found |= spec && (results << ["", ""]).concat(info).concat(spec.get_info)
+        shared_spec, shared_info = klass.map_foorth_shared_info(symbol, :shallow)
 
-        spec, info = klass.map_foorth_exclusive_info(symbol, :shallow)
-        found |= spec && (results << ["", ""]).concat(info).concat(spec.get_info)
+        if shared_spec
+          results
+            .concat([["", ""]])
+            .concat(shared_info)
+            .concat(shared_spec.get_info)
+        end
+
+        excl_spec, excl_info = klass.map_foorth_exclusive_info(symbol, :shallow)
+
+        if excl_spec
+          results
+            .concat([["", ""]])
+            .concat(excl_info)
+            .concat(excl_spec.get_info)
+        end
+
+        found ||= (shared_spec || excl_spec)
       end
 
     results << ["Scope", "not found in any class."] if symbol && !found
